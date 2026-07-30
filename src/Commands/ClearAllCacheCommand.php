274501706941f58d0f8bdcb6dcabaa538af9fdaa
Toby Allen <toby@cookingisfun.ie>
2026-07-30 07:44:32 +0100

    clear all

--- src/Commands/ClearAllCacheCommand.php
+++ src/Commands/ClearAllCacheCommand.php
@@ -3,17 +3,29 @@
 namespace Tobya\ClearAllCache\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class ClearAllCacheCommand extends Command
 {
-    public $signature = 'clearallcache';
+    public $signature = 'cache:clearall';
 
-    public $description = 'My command';
+    public $description = 'Clears default cache and other caches added to cache config.  ';
 
     public function handle(): int
     {
-        $this->comment('All done');
 
+        // clear default cache;
+        $this->info('Clearing default cache.');
+        Artisan::call('cache:clear',[], $this->output);
+
+        $cachelist = config('cache.clear_stores');
+
+        foreach ($cachelist as $cacheStore) {
+            $this->info('Clearing cache: ' . $cacheStore);
+            Artisan::call('cache:clear',[$cacheStore],$this->output);
+        }
+
+        $this->comment('All done');
         return self::SUCCESS;
     }
 }
